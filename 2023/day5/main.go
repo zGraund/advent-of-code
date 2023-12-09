d84065dd5b2bbe71e8fb3724f9a93d2ae115d0c5
zGraund <leo.gaud@hotmail.it>
2023-12-09 20:23:09 +0100

    minor day5 improvements

--- 2023/day5/main.go
+++ 2023/day5/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"slices"
 	"sort"
 	"strconv"
 	"strings"
@@ -23,13 +24,18 @@ func getNumbers(str string) []int {
 }
 
 func sortMatrix(matrix [][]int) [][]int {
+
+	/*
+		Sort the matrix based on the source value
+	*/
+
 	sort.SliceStable(matrix, func(i, j int) bool {
 		return matrix[i][1] < matrix[j][1]
 	})
 	return matrix
 }
 
-func parseInput(input string) ([]int, [][][]int) {
+func parseInput(input string) ([]int, [][][]int, [][]int) {
 
 	/*
 		Get 2 slices
@@ -37,26 +43,34 @@ func parseInput(input string) ([]int, [][][]int) {
 		1 with all the maps as a 2d array
 		convert the string numbers to int
 		and sort the matrix based on the source number
+		also make a 3rd array to use for the binary
+		search to find the corresponding range
 	*/
 
 	var seeds []int
 	var maps [][][]int
+	var indexes [][]int
 
 	t := strings.Split(input, ":")
 	seeds = getNumbers(t[1])
 	for _, v := range t[2:] {
 		var matrix [][]int
+		var index []int
 		for _, line := range strings.Split(v, "\n") {
 			if numbers := getNumbers(line); len(numbers) > 0 {
 				matrix = append(matrix, numbers)
 			}
 		}
 		maps = append(maps, sortMatrix(matrix))
+		for i := range sortMatrix(matrix) {
+			index = append(index, matrix[i][1])
+		}
+		indexes = append(indexes, index)
 	}
-	return seeds, maps
+	return seeds, maps, indexes
 }
 
-func findDestinationNumber(n int, m [][]int) int {
+func findDestinationNumber(n int, m [][]int, i []int) int {
 
 	/*
 		Since the map is sorted we can loop over it
@@ -66,19 +80,16 @@ func findDestinationNumber(n int, m [][]int) int {
 		to find the destination number
 	*/
 
+	if n < m[0][1] || n > m[len(m)-1][1]+m[len(m)-1][2] {
+		return n
+	}
 	var rang []int
-	for i, v := range m {
-		if v[1] > n {
-			if i == 0 {
-				return n
-			}
-			rang = m[i-1]
-			break
-		}
-		if i == len(m)-1 {
-			rang = v
-			break
-		}
+
+	ind, found := slices.BinarySearch(i, n)
+	if !found {
+		rang = m[ind-1]
+	} else {
+		rang = m[ind]
 	}
 	if rang[1]+rang[2] > n {
 		return rang[0] + (n - rang[1])
@@ -86,7 +97,7 @@ func findDestinationNumber(n int, m [][]int) int {
 	return n
 }
 
-func solvePart1(seeds []int, maps [][][]int) int {
+func solvePart1(seeds []int, maps [][][]int, indexes [][]int) int {
 
 	/*
 		Iterate over every seed and
@@ -101,8 +112,8 @@ func solvePart1(seeds []int, maps [][][]int) int {
 
 	result := 999999999
 	for _, source := range seeds {
-		for _, m := range maps {
-			source = findDestinationNumber(source, m)
+		for i, m := range maps {
+			source = findDestinationNumber(source, m, indexes[i])
 		}
 		if result > source {
 			result = source
@@ -111,23 +122,25 @@ func solvePart1(seeds []int, maps [][][]int) int {
 	return result
 }
 
-func solvePart2(seeds []int, maps [][][]int) int {
+func solvePart2(seeds []int, maps [][][]int, indexes [][]int) int {
 
 	/*
 		Same thing as part one but
 		this time we iterate over the
 		seeds to get the full list
 
-		THIS IS A VERY BAD BRUTEFORCE SOLUTION
+		THIS IS A VERY BAD BRUTE FORCE SOLUTION
 		~6min 30s to complete on an i5-4690
+		~3min 30s with the added check to see if
+				  the seed is in the map range
 	*/
 
 	result := 999999999
 	for i := 0; i < len(seeds); i += 2 {
 		for x := 0; x < seeds[i+1]; x++ {
 			source := seeds[i] + x
-			for _, m := range maps {
-				source = findDestinationNumber(source, m)
+			for in, m := range maps {
+				source = findDestinationNumber(source, m, indexes[in])
 			}
 			if result > source {
 				result = source
@@ -139,13 +152,13 @@ func solvePart2(seeds []int, maps [][][]int) int {
 
 // Solution 2 using go routines
 
-func mapSeed(seed int, range_ int, maps [][][]int, wg *sync.WaitGroup, ch chan int) {
+func mapSeed(seed int, range_ int, indexes [][]int, maps [][][]int, wg *sync.WaitGroup, ch chan int) {
 	defer wg.Done()
 	result := 999999999
 	for x := 0; x < range_; x++ {
 		source := seed + x
-		for _, m := range maps {
-			source = findDestinationNumber(source, m)
+		for i, m := range maps {
+			source = findDestinationNumber(source, m, indexes[i])
 		}
 		if result > source {
 			result = source
@@ -154,7 +167,7 @@ func mapSeed(seed int, range_ int, maps [][][]int, wg *sync.WaitGroup, ch chan i
 	ch <- result
 }
 
-func solvePart2GoRoutine(seeds []int, maps [][][]int) int {
+func solvePart2GoRoutine(seeds []int, maps [][][]int, indexes [][]int) int {
 
 	/*
 		Same thing as part one but
@@ -163,6 +176,8 @@ func solvePart2GoRoutine(seeds []int, maps [][][]int) int {
 
 		Still brute force but with routines
 		~3min 15s to complete on an i5-4690
+		~2min 00s with the added check to see if
+				  the seed is in the map range
 	*/
 
 	ch := make(chan int)
@@ -170,7 +185,7 @@ func solvePart2GoRoutine(seeds []int, maps [][][]int) int {
 
 	for i := 0; i < len(seeds); i += 2 {
 		wg.Add(1)
-		go mapSeed(seeds[i], seeds[i+1], maps, &wg, ch)
+		go mapSeed(seeds[i], seeds[i+1], indexes, maps, &wg, ch)
 	}
 
 	go func() {
@@ -190,10 +205,10 @@ func solvePart2GoRoutine(seeds []int, maps [][][]int) int {
 func main() {
 	i, _ := os.ReadFile("input.txt")
 	input := string(i)
-	seeds, maps := parseInput(input)
+	seeds, maps, indexes := parseInput(input)
 
-	fmt.Println("Part 1:", solvePart1(seeds, maps))
-	// fmt.Println("Part 2:", solvePart2(seeds, maps))
-	fmt.Println("Part 2:", solvePart2GoRoutine(seeds, maps))
+	fmt.Println("Part 1:", solvePart1(seeds, maps, indexes))
+	// fmt.Println("Part 2:", solvePart2(seeds, maps, indexes))
+	fmt.Println("Part 2:", solvePart2GoRoutine(seeds, maps, indexes))
 
 }
